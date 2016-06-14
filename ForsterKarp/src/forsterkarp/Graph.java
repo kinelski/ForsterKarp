89d2ec43bf5efd6955bcca0014386bf77ba62a6f
Caio Saldanha <mrcaiosaldanha@gmail.com>
2016-06-14 06:06:20 -0300

    Estrutura básica de grafo implementada.

---
+++ ForsterKarp/src/forsterkarp/Graph.java
@@ -0,0 +1,30 @@
+package forsterkarp;
+
+import java.util.ArrayList;
+
+public class Graph {
+    private int numOfNodes;
+    private int[][] adjMatrix;
+    
+    public Graph (int numOfNodes){
+        this.numOfNodes = numOfNodes;
+        adjMatrix = new int[numOfNodes][numOfNodes];
+        
+        for (int i=0; i<numOfNodes; i++)
+            for (int j=0; j<numOfNodes; j++)
+                adjMatrix[i][j] = -1;
+    }
+    
+    public int getNumOfNodes(){
+        return numOfNodes;
+    }
+    
+    public int[][] getAdjMatrix (){
+        return adjMatrix;
+    }
+    
+    public void addEdge(int node1, int node2, int weight){
+        adjMatrix[node1][node2] = weight;
+        adjMatrix[node2][node1] = weight;
+    }
+}
