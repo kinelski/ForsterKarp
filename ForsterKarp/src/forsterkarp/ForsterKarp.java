5c2695d6511286b3d145d84eb0474a7428b79d66
Gabriela Tourinho <gabrielantourinho@gmail.com>
2016-06-15 02:20:11 -0300

    Gera grafo aleatório de acordo com os requisitos do projeto

--- ForsterKarp/src/forsterkarp/ForsterKarp.java
+++ ForsterKarp/src/forsterkarp/ForsterKarp.java
@@ -3,6 +3,16 @@ package forsterkarp;
 public class ForsterKarp {
 
     public static void main(String[] args) {
+        Graph graph = new Graph(6,10);
+        
+        int[][] matrix = graph.getAdjMatrix();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+        
     }
     
 }
--- ForsterKarp/src/forsterkarp/Graph.java
+++ ForsterKarp/src/forsterkarp/Graph.java
@@ -1,18 +1,34 @@
 package forsterkarp;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Graph {
-    private int numOfNodes;
+    private int numOfNodes, numOfEdges;
     private int[][] adjMatrix;
+    private Random random = new Random();
     
-    public Graph (int numOfNodes){
+    public Graph (int numOfNodes, int numOfEdges){
         this.numOfNodes = numOfNodes;
+        this.numOfEdges = numOfEdges;
         adjMatrix = new int[numOfNodes][numOfNodes];
         
         for (int i=0; i<numOfNodes; i++)
             for (int j=0; j<numOfNodes; j++)
                 adjMatrix[i][j] = -1;
+        
+        for (int i=0; i<numOfEdges; i++){
+            int edge = random.nextInt(numOfNodes*numOfNodes);
+            int r = edge%numOfNodes;
+            int l = edge/numOfNodes;
+            if (r==l || adjMatrix[r][l]!=-1)
+                i--;
+            else{
+                addEdge(l,r,random.nextInt(10)+1);
+                /*adjMatrix[l][r] = random.nextInt(10)+1;
+                adjMatrix[r][l] = adjMatrix[l][r];*/
+            }
+        }
     }
     
     public int getNumOfNodes(){
