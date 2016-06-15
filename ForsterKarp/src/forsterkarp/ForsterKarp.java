5001c485467d1c4f08544db701a4149cfddd53eb
Caio Saldanha <mrcaiosaldanha@gmail.com>
2016-06-15 08:08:51 -0300

    Testes feitos. Resta apenas gerar o output no formato desejado para o MATLAB.

--- ForsterKarp/src/forsterkarp/ForsterKarp.java
+++ ForsterKarp/src/forsterkarp/ForsterKarp.java
@@ -1,10 +1,21 @@
 package forsterkarp;
 
+import java.io.PrintWriter;
+import java.util.Date;
+
 public class ForsterKarp {
     public static void main(String[] args) {
         
+        long start, end;
+        double av_time, av_memory;
+        PrintWriter writer = null;
+        
         Graph graph;
         HeldKarp hk = new HeldKarp();
+        
+        try{
+            writer = new PrintWriter ("out.txt", "UTF-8");
+        } catch (Exception e){}
         
         System.out.println ("CTC-20: Projeto 2");
         System.out.println ("Trabalho feito por:");
@@ -22,9 +33,51 @@ public class ForsterKarp {
             System.out.println ("Solução por Held Karp: " + hk.TSP() + "\n");
         }
         
-        System.out.println ("Teste de Desempenho Médio");
+        System.out.println ("Teste de Desempenho Médio (10 execuções)");
         System.out.println ("A saída será gerada em um arquivo out.txt no formato");
-        System.out.println ("adequado para a utilização em MATLAB.");
+        System.out.println ("adequado para a utilização em MATLAB.\n");
         
+        System.out.println ("De 3 a 20 vértices (grafo completo):");
+        for (int i=3; i<=20; i++){
+            
+            av_memory = 0.0;
+            start = (new Date()).getTime();
+            
+            for (int j=1; j<=10; j++){
+                graph = new Graph (i, (i*(i-1))/2);
+                hk.initialize(graph);
+                hk.TSP();
+                av_memory += hk.getAllocatedMemory();
+            }
+            
+            end = (new Date()).getTime();
+            av_time = (end - start)/10.0;
+            av_memory /= 10.0;
+            
+            System.out.print (i + " vértices: ");
+            System.out.print (av_time + " ms, ");
+            System.out.println (av_memory + " bytes");
+        }
+        
+        System.out.println ("\nDe 1 a 120 arestas (16 vértices):");
+        for (int i=1; i<=120; i++){
+            
+            start = (new Date()).getTime();
+            
+            for (int j=1; j<=10; j++){
+                graph = new Graph(16, i);
+                hk.initialize(graph);
+                hk.TSP();
+            }
+            
+            end = (new Date()).getTime();
+            av_time = (end - start)/10.0;
+            
+            System.out.println (i + " arestas: " + av_time + " ms");
+            
+        }
+        
+        if (writer != null)
+            writer.close();
     }
 }
